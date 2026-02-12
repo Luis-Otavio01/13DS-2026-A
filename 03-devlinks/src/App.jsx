be52cbb3894eaa9ade5eee8bbd77c4c5cf1a0b0e
Luis-Otavio01 <luisbatata2022@gmail.com>
2026-02-12 16:50:50 -0300

    feat: adicionar componentes de Link, Perfil, SocialLink e Rodape; atualizar index.html

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,22 +1,32 @@
 import './App.css'
-
+import Link from './components/Link/Link';
+import Perfil from './components/Perfil/Perfil'
+import SocialLink from './components/SocialLink/SocialLink';
 
 function App() {
-
   return (
-    <div id='App'>
-      <div className='perfil'>
-        <img src="https://placehold.co/200x200" alt="" />
-        <p>@Batata</p>
-      </div>
-      <div className="switch">
-        botão switch
-      </div>
-      <div className='links'></div>
-      <div className='socialLinks'></div>
-      <div className='rodape'></div>
-    </div>
-  )
-}
+    <div id="App">
+      <Perfil fotoPerfil={"https://placehold.co/100"}>Batata</Perfil>
 
-export default App
+      <div className="switch">botão switch</div>
+
+      <div className="links">
+      <ul>
+        <Link url={""}>Inscreva-se</Link>
+        <Link url={""}>Minha playlist</Link>
+        <Link url={""}>Me pague um café!</Link>
+        <Link url={""}>Conheça o Curso DEV</Link>
+      </ul>
+      </div>
+      <div className="socialLinks">
+      <SocialLink url={"https://github.com/"} icon={"logo-github"}/>
+      <SocialLink url={"https://instagram.com/"} icon={"logo-instagram"}/>
+      <SocialLink url={"https://youtube.com/"} icon={"logo-youtube"}/>
+      <SocialLink url={"https://br.linkedin.com/"} icon={"logo-linkedin"}/>
+       </div>
+      <div className="rodape"></div>
+    </div>
+  );
+};
+
+export default App;
--- 03-devlinks/src/components/Perfil/Perfil.jsx
+++ 03-devlinks/src/components/Perfil/Perfil.jsx
@@ -1,1 +1,12 @@
-rafce
+import React from 'react'
+
+const Perfil = ({ children, fotoPerfil}) => {
+  return (
+    <div className="perfil">
+      <img src={fotoPerfil} alt="" />
+        <p>@{children}</p>
+    </div>
+  );
+};
+
+export default Perfil;
