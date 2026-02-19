346a01a5a85e7f4d2d6c1b488565f47ee487d2ac
Luis-Otavio01 <luisbatata2022@gmail.com>
2026-02-19 09:01:19 -0300

    feat: adicionar comentários explicativos na configuração do servidor no Vite

--- 04-devflix/vite.config.js
+++ 04-devflix/vite.config.js
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
-    host: true,
+    port: 3000, ///Muda a porta padrão vite(5173) para a padrão react(3000)
+    open: true, //Abre o navegador automaticamente
+    host: true, //Permite que tenha acesso via IP na rede local
   },
 });
